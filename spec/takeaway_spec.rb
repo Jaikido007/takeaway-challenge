ca7e239e5f178995aa57e093e15e4ff1fa838cc7
Jay Mullen <81382978+Jaikido007@users.noreply.github.com>
2021-05-07 11:27:50 +0100

    User story 1: Set up a menu double to return a menu dish and price.  Injected menu into Takeaway class.

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,13 @@
 require 'takeaway'
 
 describe Takeaway do
-
+# injected menu into takeaway class
+  subject(:takeaway) { described_class.new(menu: menu) }
+# menu double
+  let(:menu) { double(:menu, print: printed_menu) }
+  let(:printed_menu) { "Soy Burger: £2.50" }
+# what menu will look like - Still to do: class Menu
+  it 'shows menu dishes with prices' do
+    expect(takeaway.print_menu).to eq(printed_menu)
+  end
 end
